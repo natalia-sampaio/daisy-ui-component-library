77001460c479ddbff57f719e4af1bbbb71fa8420
Natalia Andrade <natalia.srsa@proton.me>
2025-01-30 13:12:16 -0300

    enhancing the accordion challenge

--- packages/library/src/components/DaisyAccordionPanel/DaisyAccordionPanel.vue
+++ packages/library/src/components/DaisyAccordionPanel/DaisyAccordionPanel.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
-import { inject } from 'vue'
-
-const id = inject('accordionId', '')
-
-if (!id) {
-  throw new Error('DaisyAccordionPanel must be used inside a DaisyAccordion')
-}
+import { inject, ref } from 'vue'
 
 defineProps<{
   title: string
 }>()
+
+const id = inject('accordionId', '')
+const multipleOpen = inject('multipleOpen', false)
+const isOpen = ref(false)
+
+if (!id) {
+  throw new Error('DaisyAccordionPanel must be used inside a DaisyAccordion')
+}
 </script>
 
 <template>
   <div class="collapse collapse-arrow join-item border-base-300 border">
-    <input type="radio" :name="id" checked />
+    <input
+      :type="multipleOpen ? 'checkbox' : 'radio'"
+      :name="id"
+      :checked="isOpen"
+      @click="isOpen = !isOpen"
+    />
     <div class="collapse-title text-xl font-medium">{{ title }}</div>
     <div class="collapse-content">
       <slot></slot>
